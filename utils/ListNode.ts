2ddd3f93d3cb336c8aa42ca24912245dd5e200e2
Ming Lee Ng <mingleeng@gmail.com>
2024-02-03 20:06:04 -0700

    feat: add ListNode utility class

---
+++ utils/ListNode.ts
@@ -0,0 +1,9 @@
+export default class ListNode<T> {
+  val: T;
+  next: ListNode<T> | null;
+
+  constructor(val: T, next: ListNode<T> | null = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
