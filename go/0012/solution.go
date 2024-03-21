1da05b7c4a240d306a7e52dc0750da42fefcf2fd
Ming Lee Ng <mingleeng@gmail.com>
2024-03-21 15:31:04 -0600

    feat: add Go solution + tests for LC 0012

---
+++ go/0012/solution.go
@@ -0,0 +1,22 @@
+package problem0012
+
+import "fmt"
+
+func intToRoman(num int) string {
+	fmt.Println("What?")
+	symbols := []string{
+		"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX",
+		"V", "IV", "I",
+	}
+	values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	result := ""
+
+	for idx, val := range values {
+		for num >= val {
+			result += symbols[idx]
+			num -= val
+		}
+	}
+
+	return result
+}
